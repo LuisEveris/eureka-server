5a23878793d7c7320192c7be58c801dc26223ebd
llandaur <llandaur@everis.com>
2021-06-24 13:40:03 -0500

    Initial commit

---
+++ src/main/java/com/bootcamp/msregistrydiscoveryserver/MsRegistryDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.bootcamp.msregistrydiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsRegistryDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRegistryDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/msregistrydiscoveryserver/MsRegistryDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.msregistrydiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
